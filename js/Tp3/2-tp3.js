2111e712abfec22c804c590777e7355b21845d45
ezequielgar <ezequielgar@gmail.com>
2021-10-25 23:40:12 -0300

    nueva rama

--- js/Tp3/2-tp3.js
+++ js/Tp3/2-tp3.js
@@ -1,3 +1,4 @@
+
 // 2-  Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y almacenarlas en un arreglo, cuando el usuario seleccione cancelar o ingrese el valor “0” se debe mostrar el arreglo generado, luego realizar las siguientes acciones:
 
 // Mostrar la longitud del arreglo.
@@ -7,21 +8,39 @@
 // Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
 
 
-let cadena = [] ;
-do{
-
-    let ciudad = prompt('Ingrese una ciudad');
-    
-    if(ciudad != 0)
-    
-    if(cadena == ''){
-        cadena = cadena + ciudad ;
-            }
-    else {
-        cadena = cadena + '-' + ciudad ;
+cadena = [] ;
+ciudad = 0;
+ciudad = prompt('Ingrese una ciudad (ingrese 0 para cancelar):');
+// &&
+// V && V => V
+// F && V => F
+// V && F => F
+// F && F => F
+while (ciudad != 0 && confirm('Desea continuar?'))
+{
+    cadena.push(ciudad);
+    cadena.length;
+    // Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+    //  0         1      2    3      4      5 
+    // ["a", "ada", "sdasd", "as", "d", "elem"]
+    cadena[0];
+    if (cadena.lenght >= 3){
+        cadena[3];
     }
+    cadena[cadena.lenght - 1];
 
+    cadena[cadena.lenght - 1] = "París";
+
+
+    // if(cadena == ''){
+    //     cadena = cadena + ciudad ;
+    // }
+    // else {
+    //     cadena = cadena + '-' + ciudad ;
+    // "aadsñlkdañlsdk"
+
+    // }
+    
+    ciudad = prompt('Ingrese una ciudad (ingrese 0 para cancelar):');
 }
-while (confirm('¿Desea continuar?'));
-
 document.write(cadena);
