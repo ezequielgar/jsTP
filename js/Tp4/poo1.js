ad8aece1242350f2dfe5d0532146ac465eb6eaa0
ezequielgar <ezequielgar@gmail.com>
2021-10-31 11:21:35 -0300

    tp4-1

--- js/Tp4/poo1.js
+++ js/Tp4/poo1.js
@@ -8,11 +8,35 @@ let auto = {
     color:'blanco nacre',
     marca:'Citroen',
     modelo:'C4 Cactus Feel',
-    motorOn: 'Encendido',
-    onOff:function{
-        
+    motorOn: 'Apagado',
+    onOff:function(action){
+        let turnOnOff = action =parseInt( prompt('Eliga una opcion: 0 (Encender) - 1 (Apagar) - 2 (Cancelar)'));
+        if (!isNaN(turnOnOff && turnOnOff != null)){
 
+            switch(turnOnOff){
+                case 0:
+                    alert('Vehiculo Encendido');
+                    turnOnOff = 'Encendido';
+                    break;
+                    
+                    case 1:
+                        alert('Vehiculo Apagado');
+                        turnOnOff = 'Apagado'
+                        break;
+                        case 2:
+                            alert('Se cancela accion')
+                            break;
+                            default:
+                                alert('Accion incorrecta');            
+                            }
+                        }else {
+                            alert('Ingrese una opcion valida')
+                        }
+        return turnOnOff;
     }
-    
-
+       
 }
+
+auto.motorOn = auto.onOff();
+
+console.log(auto['motorOn'])
