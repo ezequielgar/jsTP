7b1c6891cac106296c7573de282ebbf822ac665b
Ezequiel Garcia <ezequielgar@gmail.com>
2021-10-30 21:09:04 -0300

    tp4 incio

---
+++ js/Tp4/poo1.js
@@ -0,0 +1,18 @@
+// Crear objetos
+
+// 1- Crea un objeto llamado auto que tenga algunas características como el color, marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir encender y apagar el auto.
+
+
+
+let auto = {
+    color:'blanco nacre',
+    marca:'Citroen',
+    modelo:'C4 Cactus Feel',
+    motorOn: 'Encendido',
+    onOff:function{
+        
+
+    }
+    
+
+}
