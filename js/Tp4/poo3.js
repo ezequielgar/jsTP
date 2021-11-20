255405b3fa2d892b677a23139c2f090ecfee0221
ezequielgar <ezequielgar@gmail.com>
2021-11-20 13:50:13 -0300

    poo tp4

--- js/Tp4/poo3.js
+++ js/Tp4/poo3.js
@@ -1,9 +1,40 @@
 // Rectángulos
 // 3-Escribe una clase que permita crear distintos objetos “rectángulos”, con las propiedades de alto y ancho, mas los métodos necesarios para modificar y mostrar sus propiedades, calcular el perímetro y el área
 
-
 class Rectangulos {
-    constructor(alto,ancho){
-        
-    }
+  constructor(alto, ancho) {
+    this.altura = alto;
+    this.base = ancho;
+  }
+
+
+
+  calculaSuperficie(superf){
+      superf = this.base * this.altura ;
+      console.log('La superficie es : ' + superf);
+      
+  }
+
+  calcularPerimetro(perimet){
+      perimet = this.base*2 + this.altura*2 ;
+      console.log('El perimetro es : ' + perimet);
+  }
+
+  
+
 }
+alert('Bienvenido');
+let newRectangulo = new Rectangulos();
+
+this.altura = parseInt( prompt('Ingrese la altura'));
+console.log(this.altura);
+this.base = parseInt(prompt('Ingresa la  base'))
+console.log(this.base);
+
+newRectangulo.calculaSuperficie();
+newRectangulo.calcularPerimetro();
+
+
+// falta terminar de resolver como ingresar base y altura para tomar estos datos y volcarlos en los metodos
+
+
--- js/Tp4/poo4.js
+++ js/Tp4/poo4.js
@@ -3,4 +3,28 @@
 // Posteriormente, cree tres instancias de este objeto y guárdalas en un array.
 // Por último, utilice el método imprime datos para mostrar por pantalla los valores de los tres objetos instanciados.
 
+class Productos {
+  constructor(codigo, nombre, precio) {
+    this.codigoProd = codigo;
+    this.nombProd = nombre;
+    this.precioProd = precio;
+  }
 
+  imprimeDatos() {
+    document.write(`<br>Codigo del Producto: ${this.codigoProd}
+        <br>Nombre del Producto: ${this.nombProd} <br>Precio del Producto: ${this.precioProd}`);
+  }
+}
+
+let camisa = new Productos('001','Camisa Lacoste blanca','$9000');
+let zapatillas = new Productos('002','Zapatillas Converse All Star','$5000');
+let gorra = new Productos('001','Gorra Trucker Negra','$1000');
+
+let stockProductos = [];
+stockProductos.push(camisa);
+stockProductos.push(zapatillas);
+stockProductos.push(gorra);
+
+camisa.imprimeDatos();
+gorra.imprimeDatos();
+zapatillas.imprimeDatos();
